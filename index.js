23be8423c2f668e92045796dcf38d4a8bb41b56d
Matthew Zinke <zinkem@gmail.com>
2020-05-13 12:26:58 -0700

    added module test template

--- index.js
+++ index.js
@@ -7,78 +7,85 @@
   volume mounted FAST Templates.
 
   Each template in the volume will be mapped to a URL endpoint
-  using the title in the template. 
+  using the title in the template.
 
 */
 
+const fs = require('fs');
 const http = require('http');
 
-const fs = require('fs');
 const util = require('util');
+const ls = util.promisify(fs.readdir);
+const readFile = util.promisify(fs.readFile);
+
 const yaml = require('js-yaml');
-
-const fast = require('@f5devcentral/f5-fast-core');
-
 const app = require('polka');
 
+const fast = require('@f5devcentral/f5-fast-core');
 
 const basedir = process.env.F5_FAST_TEMPLATE_ROOT || './templates';
 const templates = {};
 
-console.log(basedir);
+console.log(`basedir ${basedir}`);
 
-const ls = util.promisify(fs.readdir);
-const readFile = util.promisify(fs.readFile);
+const simpleTestPoc = (input) => input*2;
 
+const httpGetTemplate = (req, res) => {
+  const press = templates[req.params.path.toLowerCase()];
+  if(press) {
+    res.end(JSON.stringify(press._parametersSchema));
+  } else {
+    res.end('404: template not found');
+  }
+};
+
+const httpPostTemplate = (req, res) => {
+  const press = templates[req.params.path.toLowerCase()];
+  if(press) {
+    const buffer = [];
+    req.on('data', (data) => {
+      buffer.push(data.toString('utf8'));
+    });
+    req.on('end', () => {
+      const params = JSON.parse(buffer.join(''));
+      //console.log(params);
+      res.end(press.render(params));
+    });
+    req.on('error', () => {
+      res.end('500: template error');
+    });
+  } else {
+    res.end('404: template not found');
+  }
+};
+
+const server = app();
+server.get('/:path', httpGetTemplate);
+server.post('/:path', httpPostTemplate);
+
+//main promise chain listens on port 3000
 ls(basedir)
   .then((list) => {
-
     return Promise.all(list.map(fname => readFile(basedir + '/' + fname)));
-
   })
   .then((results) => {
-
     const files = results.map(x => x.toString('utf8'));
-
     return Promise.all(files.map(yaml => fast.Template.loadYaml(yaml)));
-
   })
   .then((results) => {
-
-
-
     results.forEach((template) => {
       templates[template.title.toLowerCase()] = template;
-      console.log(template.title, 'loaded');
+      //console.log(template.title, 'loaded');
     });
+    // template set loaded in cache at this point
 
-    app()
-      .get('/:path', (req, res) => {
-        const press = templates[req.params.path.toLowerCase()];
-        if(press) {
-          res.end(JSON.stringify(press._parametersSchema));
-        } else {
-          res.end('404: template not found');
-        }
-      })
-      .post('/:path', (req, res) => {
-        const press = templates[req.params.path.toLowerCase()];
-        if(press) {
-          const buffer = [];
-          req.on('data', (data) => {
-            buffer.push(data.toString('utf8'));
-          });
-          req.on('end', () => {
-            const params = JSON.parse(buffer.join(''));
-            console.log(params);
-            res.end(press.render(params));
-          });
-          req.on('error', () => {
-            res.end('500: template error');
-          });
-        } else {
-          res.end('404: template not found');
-        }
-      })
-      .listen(3000);
+    //server start
+    server.listen(3000);
   });
+
+
+module.exports = {
+  httpGetTemplate,
+  httpPostTemplate,
+  simpleTestPoc
+}
